12ef2d14334a39e7687c5a001247064539b9cc6c
Vadym <e11712168@student.tuwien.ac.at>
2021-11-09 17:08:30 +0100

    KDTree save output

--- Benchmarks/KDTree_benchmark.py
+++ Benchmarks/KDTree_benchmark.py
@@ -15,23 +15,25 @@ def benchmark_case(amount, leaf_size=1):
     stopwatch = end - start
     print('built in {} seconds'.format(stopwatch))
 
-    stopwatch += benchmark(built_hash, query[0], 1)
-    stopwatch += benchmark(built_hash, query[1], 2)
-    stopwatch += benchmark(built_hash, query[2], 3)
+    stopwatch += benchmark(built_hash, query[0], 1, amount, leaf_size)
+    stopwatch += benchmark(built_hash, query[1], 2, amount, leaf_size)
+    stopwatch += benchmark(built_hash, query[2], 3, amount, leaf_size)
 
     print('{} points case ends in {} seconds'.format(amount, stopwatch))
+    np.save('Output/KDTree/KDTree_Time_{}_{}.npy'.format(amount, leaf_size), np.array(stopwatch/3))
+
     return stopwatch
 
 
 # @profile
-def benchmark(built_hash, query, attempt):
+def benchmark(built_hash, query, attempt, amount, leaf_size=1):
     start = time.perf_counter()
     output = built_hash.search(query, 100)[1]
     end = time.perf_counter()
 
     #print(np.sort(output))
     time_result = end - start
-    np.save('', output)
+    np.save('Output/KDTree/KDTree_{}_{}_{}.npy'.format(amount, leaf_size, attempt), output)
 
     del start, end, output
     print('attempt {} ends in {} seconds'.format(attempt, time_result))
