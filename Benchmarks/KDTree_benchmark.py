46d43c6314985e1a131b16721e056c36bc958078
Vadym <e11712168@student.tuwien.ac.at>
2021-11-09 20:51:18 +0100

    update

--- Benchmarks/KDTree_benchmark.py
+++ Benchmarks/KDTree_benchmark.py
@@ -47,6 +47,6 @@ def run_benchmark():
     #stopwatch += benchmark_case(10000)
     #stopwatch += benchmark_case(100000)
     for leaf_size in range(1, 11, 3):
-        benchmark_case(10000, leaf_size)
+        benchmark_case(100000, leaf_size)
     print('"KDTree" benchmark ends in {} seconds'.format(stopwatch))
     print('===============================================================')
